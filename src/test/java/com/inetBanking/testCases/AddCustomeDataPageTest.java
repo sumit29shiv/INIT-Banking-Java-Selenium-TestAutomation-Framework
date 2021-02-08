231af4b182e0ca45e060675ba80f713d783ff377
Sumit <sumit.shivhare29@gmail.com>
2021-02-08 20:32:27 +0530

    changed testng.xml file

--- src/test/java/com/inetBanking/testCases/AddCustomeDataPageTest.java
+++ src/test/java/com/inetBanking/testCases/AddCustomeDataPageTest.java
@@ -15,7 +15,7 @@ public class AddCustomeDataPageTest extends BaseTest {
 		homePage = loginPage.doLogin(prop.getProperty("uname"),prop.getProperty("pwd"));
 		addcustDataPage = homePage.clickAddNewCustomerButton();
 		String actualMsg = addcustDataPage.addNewCustomer("Sumit","male","10","05","1998","Gandhi Nagar","Blr","KA","560102",
-				"9821114112","sumsmart123@gmail.com","123456");
+				"9821114112","sumsmart12345@gmail.com","123456");
 		Assert.assertEquals(actualMsg, "Customer Registered Successfully!!!");
 	}
 	
