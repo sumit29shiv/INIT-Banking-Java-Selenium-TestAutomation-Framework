9494839a4020ccb2baea82f1270acfe4b69ff083
Sumit <sumit.shivhare29@gmail.com>
2021-02-12 02:44:55 +0530

    updated BaseTest for headless

--- src/main/java/com/inetBanking/base/BaseTest.java
+++ src/main/java/com/inetBanking/base/BaseTest.java
@@ -12,8 +12,12 @@ import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.safari.SafariDriver;
+import org.openqa.selenium.safari.SafariOptions;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.AfterTest;
@@ -36,6 +40,8 @@ public class BaseTest {
 	public static WebDriver driver;
 	public static Logger logger;
 	public  Properties prop;
+	ChromeOptions chromeOption;
+	FirefoxOptions firefoxOption;
 	public LoginPage loginPage;
 	public HomePage homePage;
 	public AddCustomerDataPage addcustDataPage;
@@ -51,12 +57,34 @@ public class BaseTest {
 		
 		
 		if(browser.equalsIgnoreCase("chrome")) {
-			WebDriverManager.chromedriver().setup();
-			driver = new ChromeDriver();
+			if(prop.getProperty("headless").trim().equals("true")) {
+				System.out.println("Starting test in Headless mode..");
+				chromeOption = new ChromeOptions();
+				chromeOption.addArguments("--headless");
+				WebDriverManager.chromedriver().setup();
+				driver = new ChromeDriver(chromeOption);
+			}
+
+			else {
+				WebDriverManager.chromedriver().setup();
+				driver = new ChromeDriver();
+			}
+			
 		}
+		
 		else if(browser.equalsIgnoreCase("firefox")) {
-			WebDriverManager.firefoxdriver().setup();
-			driver = new FirefoxDriver();
+			if(prop.getProperty("headless").trim().equals("true")) {
+				firefoxOption = new FirefoxOptions();
+				firefoxOption.addArguments("--headless");
+				WebDriverManager.firefoxdriver().setup();
+				driver = new FirefoxDriver(firefoxOption);
+			}
+
+			else {
+				WebDriverManager.firefoxdriver().setup();
+				driver = new FirefoxDriver();
+			}
+			
 		}
 		else if(browser.equalsIgnoreCase("safari")) {
 			driver = new SafariDriver();
